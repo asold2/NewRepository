d8426959c6d291f67731b97d33a28efc6a0b1299
asold2 <74716450+asold2@users.noreply.github.com>
2020-11-27 13:58:34 +0100

    I added two new classes to the project

--- src/Car.java
+++ src/Car.java
@@ -2,9 +2,31 @@ public class Car
 {
   private String make;
   private int year;
+  private String owner;
 
   public Car(String make, int year){
     this.make = make;
     this.year = year;
   }
+
+  public String getMake()
+  {
+    return make;
+  }
+
+  public void setMake(String make)
+  {
+    this.make = make;
+  }
+
+  public int getYear()
+  {
+    return year;
+  }
+
+  public void setYear(int year)
+  {
+    this.year = year;
+  }
+
 }
